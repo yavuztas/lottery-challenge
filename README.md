13ca7df80bd36319f639accab17fa22d49b124c5
yavuztas <yavuzslmtas@gmail.com>
2025-01-24 23:30:17 +0100

    more utilized evaluate script

--- README.md
+++ README.md
@@ -9,7 +9,16 @@ brew install hyperfine
 ```
 Copy `pool.csv` file into the project's root directory.
 
-Then, run `evaluate.sh`:
+To run different solutions use classname as parameter in `evaluate.sh <classname>`:
 ```
-./evaluate.sh
+./evaluate.sh Main
 ```
+## Solutions
+
+### Using Java Memory Api (preview in Java 21)
+Run `evaluate.sh Main`. This solution doesn't work in native mode since Graalvm doesn't support Java Memory Api for ARM platforms yet. It should work in AMD x64 platforms, but I haven't tried it.
+
+### Using UNSAFE
+Run `evaluate.sh MainUnsafe`. This solution is just to push the limits for fun. It is also fully compatible and works in Graalvm Native.
+For native run `evaluate.sh MainUnsafe --native`
+
--- evaluate.sh
+++ evaluate.sh
@@ -3,28 +3,39 @@
 JAVA_VERSION="21.0.5-graal"
 INPUT_ARGS="9 13 40 34 17 7" # 9 13 40 34 17 7
 
-# java compile
-"$HOME"/.sdkman/candidates/java/$JAVA_VERSION/bin/javac --release 21 --enable-preview -d ./bin ./src/Main.java
-
 # Handle positional arguments
 shift $((OPTIND - 1))
 param1=$1
+param2=$2
 
-if [ "$param1" == "--native" ]; then
-    NATIVE_IMAGE_OPTS="--initialize-at-build-time=MainUnsafe --gc=epsilon -O3 -march=native -R:MaxHeapSize=128m -H:-GenLoopSafepoints --enable-preview"
-    native-image $NATIVE_IMAGE_OPTS -cp ./bin MainUnsafe
+if [ "$param1" = "" ]; then
+  echo "Usage: $0 <classname>"
+  exit 1
+fi
+
+echo "using $JAVA_VERSION"
+
+# set java version
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java $JAVA_VERSION
+
+# java compile
+"$HOME"/.sdkman/candidates/java/$JAVA_VERSION/bin/javac --release "$(echo $JAVA_VERSION | cut -d. -f1)" --enable-preview -d ./bin ./src/"$param1".java
+
+if [ "$param2" == "--native" ]; then
+    NATIVE_IMAGE_OPTS="--initialize-at-build-time=$param1 --gc=epsilon -O3 -march=native -R:MaxHeapSize=128m -H:-GenLoopSafepoints --enable-preview"
+    native-image $NATIVE_IMAGE_OPTS -cp ./bin "$param1"
 fi
 
 TIMEOUT="gtimeout -v 30" # in seconds, from `brew install coreutils`
-HYPERFINE_OPTS="--warmup 5 --runs 5 --export-json timing.json --output ./main.out" # --show-output 
+HYPERFINE_OPTS="--warmup 5 --runs 5 --export-json timing.json --output ./$param1.out" # --show-output
 
-if [ "$param1" == "--native" ]; then
-    echo "Picking up existing native image './mainunsafe', delete the file to select JVM mode." 1>&2
-    hyperfine $HYPERFINE_OPTS "$TIMEOUT ./mainunsafe $INPUT_ARGS"
+if [ "$param2" == "--native" ]; then
+    imageName=$(echo "$param1" | tr '[:upper:]' '[:lower:]')
+    echo "Picking up native image './$imageName'" 1>&2
+    hyperfine $HYPERFINE_OPTS "$TIMEOUT ./$imageName $INPUT_ARGS"
 else
     JAVA_OPTS="-Xmx64m --enable-preview"
-    echo "Choosing to run the app in JVM mode as no native image was found" 1>&2
-    hyperfine $HYPERFINE_OPTS "$TIMEOUT sh -c '$HOME/.sdkman/candidates/java/$JAVA_VERSION/bin/java $JAVA_OPTS -classpath ./bin Main $INPUT_ARGS'"
+    echo "Choosing to run the app in JVM mode" 1>&2
+    hyperfine $HYPERFINE_OPTS "$TIMEOUT sh -c '$HOME/.sdkman/candidates/java/$JAVA_VERSION/bin/java $JAVA_OPTS -classpath ./bin $param1 $INPUT_ARGS'"
 fi
-
-# 10596688547
