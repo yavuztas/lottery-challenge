b20915a8c880e7d46f61b247541baef985d50011
yavuztas <yavuzslmtas@gmail.com>
2025-01-25 19:17:10 +0100

    update graalvm version

--- evaluate.sh
+++ evaluate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JAVA_VERSION="21.0.5-graal"
+JAVA_VERSION="21.0.6-graal"
 INPUT_ARGS="9 13 40 34 17 7" # 9 13 40 34 17 7
 
 # Handle positional arguments
@@ -23,7 +23,7 @@ sdk use java $JAVA_VERSION
 "$HOME"/.sdkman/candidates/java/$JAVA_VERSION/bin/javac --release "$(echo $JAVA_VERSION | cut -d. -f1)" --enable-preview -d ./bin ./src/"$param1".java
 
 if [ "$param2" == "--native" ]; then
-    NATIVE_IMAGE_OPTS="--initialize-at-build-time=$param1 --gc=epsilon -O3 -march=native -R:MaxHeapSize=128m -H:-GenLoopSafepoints --enable-preview"
+    NATIVE_IMAGE_OPTS="--initialize-at-build-time=$param1 -O3 -march=native -R:MaxHeapSize=128m -H:-GenLoopSafepoints --enable-preview" # --gc=epsilon
     native-image $NATIVE_IMAGE_OPTS -cp ./bin "$param1"
 fi
 
